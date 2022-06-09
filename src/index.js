ff0e92c755b278c14875a02b51212a004f6b1c02
thihuli <thiagocandidof@hotmail.com>
2022-06-09 16:44:24 -0300

    feat: add service create account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+app.use(express.json())
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  console.log(customers)
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return response.status(201).send("Conta criada com sucesso")
+
+});
 
 app.listen(3333);
